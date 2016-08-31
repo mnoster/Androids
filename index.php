d9f02bb60811c3927742b9e42a2711d5f614dc46
Nicholas Porter <njporter10@gmail.com>
2016-08-30 22:53:51 -0700

    added in login and registration pages

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
     <title>Androids</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script src="soundcloud.js"></script>
 
 </head>
 <body>
@@ -34,6 +37,7 @@
         </div>
     </div>
 </nav>
+<div class="container">
 <div class="container main-contain">
     <div class="row">
         <div class="col-xs-12 main-head">
@@ -43,17 +47,25 @@
 </div>
 <div class="container">
     <div class="row">
-        <div id ="left-nav" class="col-xs-3 gen-style">
-            <h3>Squad</h3>
+        <div id="left-nav" class="col-xs-3 gen-style">
+            <h5><i class="fa fa-spinner w3-spin" style="font-size:64px"></i>
+            </h5>
             <hr>
-            Hey
+            <input id="sc-search" type="text" class="form-control">
+            <button id="submit">search</button>
             <hr>
-            you
+            <div class='contain-player'>
+                <audio controls>
+                    <source src="songs/Tell%20Me.mp3" type="audio/mpeg">
+                    <source src="songs/Me%20de%20Amor.mp3" type="audio/mpeg">
+                    Your browser does not support the audio element.
+                </audio>
+<!--                <div id="SCplayer"></div>-->
+            </div>
         </div>
         <div id="main-info" class="col-xs-8 gen-style"></div>
     </div>
 </div>
+</div>
 
-
-</body>
 </html>
