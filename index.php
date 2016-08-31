df3e0791a0344816feb4d8bbd62cefdbff977b52
Nicholas Porter <njporter10@gmail.com>
2016-08-30 21:32:33 -0700

    skeleton being built out

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Androids</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
@@ -11,25 +13,17 @@
 <body>
 <nav class="navbar navbar-inverse">
     <div class="contatiner-fluid">
-        <div class="navbar-header">
+        <div class="navbar-header navigation-titles">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href=index.html>Androids</a>
+            <a class="navbar-brand" href=index.php>Androids</a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Home</a></li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Research <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Tools</a></li>
-                        <li><a href="Form.html">Client Form</a></li>
-                        <li><a href="#">Page 1-3</a></li>
-                    </ul>
-                </li>
                 <li><a href="About.html">About</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
@@ -40,6 +34,26 @@
         </div>
     </div>
 </nav>
+<div class="container main-contain">
+    <div class="row">
+        <div class="col-xs-12 main-head">
+            <h1>Android Space</h1>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div id ="left-nav" class="col-xs-3 gen-style">
+            <h3>Squad</h3>
+            <hr>
+            Hey
+            <hr>
+            you
+        </div>
+        <div id="main-info" class="col-xs-8 gen-style"></div>
+    </div>
+</div>
+
 
 </body>
 </html>
