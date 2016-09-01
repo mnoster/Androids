400c96718e09c077fb5eebbd7f372308c107a310
Nicholas Porter <njporter10@gmail.com>
2016-09-01 12:16:49 -0700

    added old commit

--- login_handler.php
+++ login_handler.php
@@ -17,19 +17,22 @@ mysqli_query($conn,$query);
 
 $rows_affected = mysqli_affected_rows($conn);
 
-$result = mysqli_query($conn,$query);
+//$result = mysqli_query($conn,$query);
 //print('<pre>' .print_r($result,true) . '</pre>');
-if(mysqli_num_rows($result) > 0){
-    while($row = mysqli_fetch_assoc($result)){
-//        print_r($row, true);
-        $arrayy[]=$row;
+//if(mysqli_num_rows($result) > 0){
+//    while($row = mysqli_fetch_assoc($result)){
+////        print_r($row, true);
+//        $arrayy[]=$row;
+//
+//    }
+//    $arrayy = json_encode($arrayy);
+//        print($arrayy);
+//    $_SESSION['username'] = $row['username'];
+//        print_r($_SESSION);
+//}
 
-    }
-    $arrayy = json_encode($arrayy);
-        print($arrayy);
-    $_SESSION['username'] = $row['username'];
-        print_r($_SESSION);
-}
+
+
 
 
 if($rows_affected > 0){
@@ -44,3 +47,6 @@ if($rows_affected > 0){
     print($output);
 }
 ?>
+
+<!--//when you have a query that is frequently being used you should encrypt and cache the response-->
+<!--//use the php apc functions -->
