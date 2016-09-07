3f3f41eec27070687f9796f32e411379215eb392
Nicholas Porter <njporter10@gmail.com>
2016-09-06 17:59:08 -0700

    added license info

--- create_profile.php
+++ create_profile.php
@@ -19,7 +19,7 @@ if (empty($_SESSION)) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="user_list.js"></script>
-    <script src="add_friend.js"></script>
+<!--    <script src="add_friend.js"></script>-->
 
 <body style="background-color:transparent">
 <nav class="navbar navbar-inverse">
