c6438822f0cb67fd107f527c535cf2028e2600b1
Nicholas Porter <njporter10@gmail.com>
2016-09-07 12:46:26 -0700

    added a mailer handler, need to next create the contact page

---
+++ php_mailer/mail_handler.php
@@ -0,0 +1,46 @@
+<?php
+require_once('email_config.php');
+require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+
+
+$mail->Username = EMAIL_USER;                 // SMTP username
+$mail->Password = EMAIL_PASS;                 // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+$options = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+$mail->smtpConnect($options);
+$mail->From = addslashes($_POST['email']);//your email sending account
+$mail->FromName = addslashes($_POST['name']); //your email sending account name
+$mail->addAddress(EMAIL_USER);     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo($mail->From);
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = $_POST['subject'];
+$mail->Body    = htmlentities($_POST['content']);
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo json_encode('Message has been sent');
+}
+?>
