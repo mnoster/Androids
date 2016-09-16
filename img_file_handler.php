24b837d47875eff27d633ea3bb3ba423049e4523
Nicholas Porter <njporter10@gmail.com>
2016-09-15 23:54:27 -0700

    added in the new make dir

--- img_file_handler.php
+++ img_file_handler.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 $username = $_SESSION['username'];
-$target_dir = '/user_image/'; //variable to hold target directory
+//$target_dir = '/user_image/'; //variable to hold target directory
+$target_dir = mkdir('user_image');
 $target_file = $target_dir . $_FILES['upload_file']['name'];// this will create a file path using directory
 $user_img_file = $_FILES['upload_file'];
 ?>
